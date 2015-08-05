6734e3b8d9904f60dd6b49a76a369b77e8bc56d9
Adam Saegebarth <adams@tune.com>
2015-08-05 14:53:52 -0700

    support CORS for stubbed endpoints

--- lib/committee/middleware/stub.rb
+++ lib/committee/middleware/stub.rb
@@ -8,7 +8,12 @@ module Committee::Middleware
 
     def handle(request)
       if link = @router.find_request_link(request)
-        headers = { "Content-Type" => "application/json" }
+        headers = {
+          "Content-Type" => "application/json",
+          "Access-Control-Allow-Origin" => '*',
+          "Access-Control-Allow-Methods" => "GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD",
+          "Access-Control-Allow-Headers" => "*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match",
+        }
         data = cache(link.method, link.href) do
           Committee::ResponseGenerator.new.call(link)
         end
